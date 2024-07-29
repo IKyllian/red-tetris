cf7f1ff70aff15387a07601016b9c70242a6d000
IKyllian <kyllian.delporte@gmail.com>
2024-07-29 21:16:21 +0200

    lobby test

--- back/src/lobby/lobby.spec.ts
+++ back/src/lobby/lobby.spec.ts
@@ -1,4 +1,5 @@
 import { Lobby } from "./lobby"
+import { GameSocketManager } from '../game/game-socket-manager';
 
 describe('lobby', () => {
     let lobby: Lobby
@@ -6,6 +7,15 @@ describe('lobby', () => {
     beforeEach(() => {
         lobby = new Lobby('Test Lobby', 'Test Player', 'testPlayerId')
     })
+
+    // afterEach(() => {
+    //     if (lobby.game) {
+    //         lobby.players.forEach(({id}) => {
+    //             lobby.game.leave(id)
+    //         }) // Ensure the game is stopped
+    //     }
+    //     jest.clearAllMocks(); // Clear mocks to avoid test contamination
+    // });
 
     describe('createRandomId', () => {
         it('should create a random seed of the specified length', () => {
@@ -56,6 +66,61 @@ describe('lobby', () => {
             lobby.gameStarted = true
             lobby.addPlayer('asdasdasd', 'asdasd')
             expect(lobby.players.length).toBe(1)
+        })
+    })
+
+    describe('deletePlayer', () => {
+        it('should remove player from the lobby', () => {
+            lobby.addPlayer('player2', 'player2')
+            expect(lobby.players.length).toBe(2)
+            lobby.deletePlayer('player2')
+            expect(lobby.players.length).toBe(1)
+        })
+    })
+
+    describe('startGame', () => {
+        const mockServer = {
+            to: jest.fn().mockReturnThis(),
+            emit: jest.fn(),
+        };
+        const mockGameSocketManager = {
+            setGameToSocket: jest.fn(),
+        } as any as GameSocketManager;
+
+        const server = mockServer as any;
+        
+
+        it('should start the game', () => {
+            lobby.addPlayer('player2', 'player2')
+            lobby.startGame(server, mockGameSocketManager)
+            lobby.players.forEach(({ id }) => {
+                expect(mockGameSocketManager.setGameToSocket).toHaveBeenCalledWith(id, lobby.game)
+            })  
+        })
+    })
+
+    describe('getPlayer', () => {
+        it('should return player from the lobby', () => {
+            lobby.addPlayer('player2', 'player2')
+            const player = lobby.getPlayer('player2')
+            expect(player).toBeTruthy()
+            expect(player?.name).toBe('player2')
+        })
+
+        it('should return undefined if player does not exist in the lobby', () => {
+            const player = lobby.getPlayer('player2')
+            expect(player).toBeUndefined()
+        })
+    })
+
+    describe('getInfos', () => {
+        it('should return lobby info', () => {
+            const info = lobby.getInfo()
+            expect(info).toHaveProperty('id')
+            expect(info).toHaveProperty('name')
+            expect(info).toHaveProperty('players')
+            expect(info).toHaveProperty('gameStarted')
+            expect(info).toHaveProperty('maxPlayers')
         })
     })
 })
